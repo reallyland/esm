67c85a968495e86a980f90c477e934a0f0f3d113
Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>
2022-03-18 22:54:16 +0800

    chore: update project config
    Signed-off-by: Rong Sen Ng (motss) <wes.ngrongsen@gmail.com>

---
+++ postinstall.mjs
@@ -0,0 +1,32 @@
+#!/usr/bin/env zx
+
+const {
+  CI = 'false',
+  INIT_CWD = '',
+} = process.env;
+
+if (CI !== 'true') {
+  const {
+    name: moduleName,
+  } = JSON.parse(await fs.readFile('./package.json', { encoding: 'utf-8' }));
+
+  if (
+    [
+      moduleName,
+      moduleName.split('/', 2)[1]
+    ].some(n => (
+      !INIT_CWD.endsWith(`node_modules/${n}`) &&
+      INIT_CWD.endsWith(n)
+    ))
+  ) {
+    /**
+     * NOTE: To skip running `simple-git-hooks` in CI environment.
+     * But `npm x -y -- simple-git-hooks@latest` does not work as expected so splitting it into
+     * a 2-step process: install without saving as dependency then execute it.
+     */
+    await $`npm i --no-save simple-git-hooks`
+    await $`simple-git-hooks`;
+
+    await $`npm dedupe`;
+  }
+}
